ee77c210d3500e61161c196cfac5d09b17d2a616
Dif00l <55057915+Dif00l@users.noreply.github.com>
2024-03-08 15:30:16 +0100

    Create Select.jsx

---
+++ components/Select/Select.jsx
@@ -0,0 +1,8 @@
+function Select(props){
+    return(
+        <select style={{width:"120px",height:"40px",fontSize:"12pt",lineHeight:"1.5",fontWeight:"bold"}} onChange={props.onselect}>
+            <option></option>
+            <option value="Dif00l">Dif00l</option>
+        </select>
+    )
+}
